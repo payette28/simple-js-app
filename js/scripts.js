bb710f9d569e19d3c57ed4f55eece8eb11ec10ff
payette28 <133899413+payette28@users.noreply.github.com>
2023-05-29 17:19:26 -0400

    Update scripts.js
    if applied, formatted the code and added a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
-let name = 'pokemonList';
-let pokemonList=[{name:'Bulbasaur', height: 3, type: ["grass","monster"]},{name: "Eveee" , height: 2 , type: ["normal","field"]},{name: "Sandshrew" , height: 4 , type: ["ground","field"]}];
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, type: ["grass", "monster"] },
+    { name: "Eveee", height: 0.3, type: ["normal", "field"] },
+    { name: "Sandshrew", height: 1.2, type: ["ground", "field"] }
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 1 && pokemonList[i].height > 0.7) {
+        document.write(pokemonList[i].name + " is an average sized pokemon");
+    } else if (pokemonList[i].age < 0.7) {
+        document.write(pokemonList[i].name + " is a small pokemon");
+    } else {
+        document.write(pokemonList[i].name + " Wow, that's big!");
+    }
+}
