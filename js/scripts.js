f14a113161db1c279bbdab14a65be011bdc3cdc5
payette28 <133899413+payette28@users.noreply.github.com>
2023-05-30 15:36:47 -0400

    Update scripts.js
    if applied, fixes typo on Eevee, and the for loop to work properly, while also adding heights to the output

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, type: ["grass", "monster"] },
-    { name: "Eveee", height: 0.3, type: ["normal", "field"] },
+    { name: "Eevee", height: 0.3, type: ["normal", "field"] },
     { name: "Sandshrew", height: 1.2, type: ["ground", "field"] }
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 1 && pokemonList[i].height > 0.7) {
-        document.write(pokemonList[i].name + " is an average sized pokemon");
-    } else if (pokemonList[i].age < 0.7) {
-        document.write(pokemonList[i].name + " is a small pokemon");
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height < 1 && pokemonList[i].height > 0.6) {
+        document.write(pokemonList[i].name + " height ", pokemonList[i].height +  " This is an average sized pokemon. ");
+    } else if (pokemonList[i].height < 0.6){
+        document.write(pokemonList[i].name + " height ", pokemonList[i].height + " This a small pokemon. ");
     } else {
-        document.write(pokemonList[i].name + " Wow, that's big!");
+        document.write(pokemonList[i].name + " height ", pokemonList[i].height + " Wow, that's big! "); 
     }
 }
