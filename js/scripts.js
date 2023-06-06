d22741fdcb07192a5476a9a4ece007d60c266095
payette28 <133899413+payette28@users.noreply.github.com>
2023-06-06 18:15:43 -0400

    Update scripts.js
    if applied, should still run the same as before but now with a forEach function and adding an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,53 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, type: ["grass", "monster"] },
-    { name: "Eevee", height: 0.3, type: ["normal", "field"] },
-    { name: "Sandshrew", height: 1.2, type: ["ground", "field"] }
-];
+let pokemonRepository = (function () {
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 1 && pokemonList[i].height > 0.6) {
-        document.write(pokemonList[i].name + " height ", pokemonList[i].height + " This is an average sized pokemon. ")
-        document.write("<br>");
-    } else if (pokemonList[i].height < 0.6) {
-        document.write(pokemonList[i].name + " height ", pokemonList[i].height + " This a small pokemon. ");
-        document.write("<br>");
-    } else {
-        document.write(pokemonList[i].name + " height ", pokemonList[i].height + " Wow, that's big! ");
-        document.write("<br>");
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            type: ["grass", "monster"]
+        },
+
+        {
+            name: "Eevee",
+            height: 0.3,
+            type: ["normal", "field"]
+        },
+
+        {
+            name: "Sandshrew",
+            height: 1.2,
+            type: ["ground", "field"]
+        }
+
+    ]
+    pokemonList.forEach(function (pokemon) {
+        if (pokemon.height < 1 && pokemon.height > 0.6) {
+            document.write(pokemon.name + " height ", pokemon.height + " this is an average sized pokemon ")
+            document.write('<br>')
+        }
+        else if (pokemon.height < 0.6) {
+            document.write(pokemon.name + " height ", pokemon.height + " This a small pokemon. ");
+            document.write("<br>");
+        } else {
+            document.write(pokemon.name + " height ", pokemon.height + " Wow, that's big! ");
+            document.write("<br>");
+        
     }
+    });
+
+function getAll() {
+    return pokemonList;
 }
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+return {
+    getAll: getAll,
+    add: add
+}
+
+}) ();
+
+document.write(pokemonRepository.getAll().forEach(pokemonList));
