d393bc2b9f336adf15f8aa349b937b86fe3d0177
payette28 <133899413+payette28@users.noreply.github.com>
2023-06-07 17:49:44 -0400

    Update scripts.js
    if applied moves list outside of the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -20,20 +20,22 @@ let pokemonRepository = (function () {
         }
 
     ]
-    pokemonList.forEach(function (pokemon) {
-        if (pokemon.height < 1 && pokemon.height > 0.6) {
-            document.write(pokemon.name + " height ", pokemon.height + " this is an average sized pokemon ")
-            document.write('<br>')
-        }
-        else if (pokemon.height < 0.6) {
-            document.write(pokemon.name + " height ", pokemon.height + " This a small pokemon. ");
-            document.write("<br>");
-        } else {
-            document.write(pokemon.name + " height ", pokemon.height + " Wow, that's big! ");
-            document.write("<br>");
+    // pokemonList.forEach(function (pokemon) {
+    //     if (pokemon.height < 1 && pokemon.height > 0.6) {
+    //         document.write(pokemon.name + " height ", pokemon.height + " this is an average sized pokemon ")
+    //         document.write('<br>')
+    //     }
+    //     else if (pokemon.height < 0.6) {
+    //         document.write(pokemon.name + " height ", pokemon.height + " This a small pokemon. ");
+    //         document.write("<br>");
+    //     } else {
+    //         document.write(pokemon.name + " height ", pokemon.height + " Wow, that's big! ");
+    //         document.write("<br>");
         
-    }
-    });
+    // }
+    // }
+    // )
+    // ;
 
 function getAll() {
     return pokemonList;
@@ -50,4 +52,24 @@ return {
 
 }) ();
 
-document.write(pokemonRepository.getAll().forEach(pokemonList));
+var newPokemonList = pokemonRepository.getAll();    // Is this what you mean by new variable?
+
+
+// Unsure if you mean this foreach loop to be outside the let function or if I was supposed to leave it inside. What's the difference?
+
+newPokemonList.forEach(function (pokemon) {
+    if (pokemon.height < 1 && pokemon.height > 0.6) {
+        document.write(pokemon.name + " height ", pokemon.height + " this is an average sized pokemon ")
+        document.write('<br>')
+    }
+    else if (pokemon.height < 0.6) {
+        document.write(pokemon.name + " height ", pokemon.height + " This a small pokemon. ");
+        document.write("<br>");
+    } else {
+        document.write(pokemon.name + " height ", pokemon.height + " Wow, that's big! ");
+        document.write("<br>");
+    
+}
+}
+)
+;
